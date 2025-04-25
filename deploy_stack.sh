c46d2f704d074fc983ca341139d6fa96a06d8d2a
RohitChauriha <rohitchauriha@gmail.comm>
2025-04-26 03:10:29 +0530

    first-cut

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -3,6 +3,7 @@ docker build -t front-end-image -f Dockerfile .
 cd ../back-end-demo
 docker build -t back-end-image -f Dockerfile .
 cd ../
+docker swarm init
 docker stack deploy -c start-app.yml start-app
 # docker service scale <service-name>=<count>
 # Test the app using http://localhost:5000/hello
