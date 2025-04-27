4dc3a2e222769f9affb19e5023a51962c5243f45
RohitChauriha <rohitchauriha@gmail.comm>
2025-04-27 23:11:11 +0530

    added logging

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -3,8 +3,8 @@ docker build -t front-end-image -f Dockerfile .
 cd ../back-end-demo
 docker build -t back-end-image -f Dockerfile .
 cd ../
-docker compose up
-#docker swarm init
-#docker stack deploy -c docker-compose.yml start-app
+#docker compose up
+docker swarm init
+docker stack deploy -c docker-compose.yml start-app
 # docker service scale <service-name>=<count>
 # Test the app using http://localhost:5000/hello
