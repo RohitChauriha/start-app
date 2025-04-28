6c1a52fdc0711547a82ef42e5af0f37050c09ee5
RohitChauriha <rohitchauriha@gmail.comm>
2025-04-28 22:43:56 +0530

    added logging

--- back-end-demo/src/main/java/com/example/backend/back_end_demo/services/HelloController.java
+++ back-end-demo/src/main/java/com/example/backend/back_end_demo/services/HelloController.java
@@ -3,16 +3,29 @@ package com.example.backend.back_end_demo.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
 public class HelloController {
+    public static String hostname = System.getenv("HOSTNAME");
 
     @GetMapping("/hello")
-    public String sayHello() {
-        log.info("hello world started");
-        return "Hello World";
+    public Map<String, String> sayHello() throws UnknownHostException {
+        String containerId = InetAddress.getLocalHost().getHostName();
+        String containerAddress = InetAddress.getLocalHost().getHostAddress();
+        log.info("backend - hostname: {}, IP: {}", containerId, containerAddress);
+        Map<String, String> result = new HashMap<>();
+        result.put("hostname", containerId);
+        result.put("ip", containerAddress);
+        return result;
     }
 
     @GetMapping("/")
