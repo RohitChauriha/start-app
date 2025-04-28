1b13ca605c22f9b1aa6772a09cbf1d9d31e3a82b
RohitChauriha <rohitchauriha@gmail.comm>
2025-04-28 23:22:05 +0530

    added logging

--- back-end-demo/src/main/java/com/example/backend/back_end_demo/services/HelloController.java
+++ back-end-demo/src/main/java/com/example/backend/back_end_demo/services/HelloController.java
@@ -3,8 +3,6 @@ package com.example.backend.back_end_demo.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
