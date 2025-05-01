9f08e0beda264d904984efb3422ae69a63f6ef1b
RohitChauriha <rohitchauriha@gmail.comm>
2025-05-01 19:33:27 +0530

    added build script

---
+++ build-and-push-images.sh
@@ -0,0 +1,12 @@
+cd ./back-end-demo
+mvn clean install
+if [ $? -ne 0 ]; then
+  echo "Command failed with exit status: $?"
+  exit ;
+fi
+docker build -t rodoch/back-end-image -f Dockerfile .
+cd ../front-end-demo
+docker build -t rodoch/front-end-image -f Dockerfile .
+docker login --username=rodoch
+docker push rodoch/back-end-image
+docker push rodoch/front-end-image
--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,12 +1,4 @@
-cd ./front-end-demo
-docker build -t front-end-image -f Dockerfile .
-cd ../back-end-demo
-docker build -t back-end-image -f Dockerfile .
 mkdir -p /var/log/start-app/frontend
 mkdir -p /var/log/start-app/backend
-cd ../
-#docker compose up
-#docker swarm init
+docker swarm init
 docker stack deploy -c docker-compose.yml start-app
-# docker service scale <service-name>=<count>
-# Test the app using http://localhost:5000/hello
