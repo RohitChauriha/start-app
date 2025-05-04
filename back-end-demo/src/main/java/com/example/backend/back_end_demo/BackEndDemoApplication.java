64ebba1bf557b55200a2c5a2dff8da57139695ab
RohitChauriha <rohitchauriha@gmail.comm>
2025-05-04 17:34:43 +0530

    added book controller

--- back-end-demo/src/main/java/com/example/backend/back_end_demo/BackEndDemoApplication.java
+++ back-end-demo/src/main/java/com/example/backend/back_end_demo/BackEndDemoApplication.java
@@ -1,16 +1,12 @@
 package com.example.backend.back_end_demo;
 
-import com.example.backend.back_end_demo.domain.Book;
-import com.example.backend.back_end_demo.services.BookService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.net.InetAddress;
 
 
 @SpringBootApplication
@@ -21,19 +17,21 @@ public class BackEndDemoApplication {
         SpringApplication.run(BackEndDemoApplication.class, args);
     }
 
-//    @Autowired
+    //    @Autowired
 //    BookService bookService;
-//
-//    @Bean
-//    public CommandLineRunner startup() {
-//
-//        return args -> {
+    @Bean
+    public CommandLineRunner startup() {
+
+        return args -> {
+            String containerId = InetAddress.getLocalHost().getHostName();
+            String containerAddress = InetAddress.getLocalHost().getHostAddress();
+            System.setProperty("HOSTNAME", containerId);
+            System.setProperty("IPAddr", containerAddress);
 //            bookService.create(Book.builder().title("First Book").authors(Arrays.asList("author1", "author2")).createdAt(LocalDateTime.now()).modifiedAt(LocalDateTime.now()).build());
 //            bookService.create(Book.builder().title("Second Book").authors(Arrays.asList("author2", "author3")).createdAt(LocalDateTime.now()).modifiedAt(LocalDateTime.now()).build());
 //            System.out.println("Database initialized!");
 //            Iterable<Book> books = bookService.findAll();
 //            books.forEach(System.out::println);
-//
-//        };
-//    }
+        };
+    }
 }
