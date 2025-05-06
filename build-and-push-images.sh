5d7764f0ab44c82efea67ebf9bf231ab010d635d
RohitChauriha <rohitchauriha@gmail.comm>
2025-05-06 18:18:13 +0530

    added networks for each service

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -14,7 +14,10 @@ fi
 docker build -t rodoch/consumer-service-image:latest -f Dockerfile .
 cd ../front-end-demo
 docker build -t rodoch/front-end-image:latest -f Dockerfile .
+cd ../message-queue
+docker build -t rodoch/message-queue-image:latest -f Dockerfile .
 docker login --username=rodoch
 docker push rodoch/back-end-image
 docker push rodoch/front-end-image
 docker push rodoch/consumer-service-image
+docker push message-queue-image
