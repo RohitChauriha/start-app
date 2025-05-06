aa7e06793d5eb682e82172d5752d38eeeea25a95
RohitChauriha <rohitchauriha@gmail.comm>
2025-05-06 12:38:37 +0530

    added networks for each service

--- undeploy.sh
+++ undeploy.sh
@@ -3,4 +3,4 @@ docker swarm leave --force
 docker rm -f $(docker ps -aq)
 docker network rm docker_gwbridge
 docker image prune -f
-#docker volume rm start-app_db-data
+docker volume rm $(docker volume ls -q)
